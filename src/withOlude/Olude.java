99da7fe41eedb99fa7c3a9b4afc50551e0ff5519
Olude Adeogun <O_ADEOGU@encs.concordia.ca>
2017-12-18 18:13:28 -0500

    Format method added by olude

--- src/withOlude/Olude.java
+++ src/withOlude/Olude.java
@@ -4,11 +4,31 @@ public class Olude {
 
 	
 public static void main(String [] args){
- System.out.println("Hi Olude");		
+ System.out.println("Hi Olude");	
+ format("5142080522");	
 }
 
 public void test(){
 	System.out.println("Hi Buyoung");
+}
+
+public static void format(String phoneNum){
+
+if (phoneNum.length() != 10){
+	System.out.println("Number not complete!!!");
+}
+else{
+	String areaCode, zone, num;
+	areaCode = phoneNum.substring(0,3);
+	zone = phoneNum.substring(3,6);
+	num = phoneNum.substring(5,10);
+
+	System.out.println(areaCode+"-"+zone+"-"+num );
+
+}
+
+
+
 }
 	
 }
